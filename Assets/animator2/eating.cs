16c2b0784189de972303a9394fb31403f8489816
Scott Goerzen <sgoerzen@uvic.ca>
2017-03-29 13:48:43 -0700

    Added player fish and fish animations

---
+++ Assets/animator2/eating.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class eating : MonoBehaviour {
+    public Animator ano;
+    public int timer;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (timer < 10)
+        {
+            ano.SetBool("eating", false);
+        }
+        else
+        {
+            timer--;
+        }
+        
+		
+	}
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "enemy")
+        {
+            ano.SetBool("eating", true);
+            timer = 30;
+        }
+
+
+    }
+}
