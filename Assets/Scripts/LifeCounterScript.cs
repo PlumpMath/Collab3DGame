bd467abdc2f853b6e70c67adf0be388761b878a6
Scott Goerzen <sgoerzen@uvic.ca>
2017-04-09 15:20:47 -0700

    Life counters spawn as children of camera

--- Assets/Scripts/LifeCounterScript.cs
+++ Assets/Scripts/LifeCounterScript.cs
@@ -4,24 +4,23 @@ using UnityEngine;
 
 public class LifeCounterScript : MonoBehaviour {
 
-	private GameObject player;
+	public GameObject player;
 	public GameObject lifeCounter;
 
 	// Use this for initialization
 	void Start () {
-		player = GameObject.FindGameObjectWithTag ("Player");
+		//player = GameObject.FindGameObjectWithTag ("Main Camera");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 pos = player.transform.position;
 	}
 
 	public GameObject spawnLife (int num) {
 		var temp = Instantiate (lifeCounter);
 		temp.transform.parent = player.transform;
 		Vector3 pos = temp.transform.position;
-		pos.y -= (num - 1) * 10;
+		pos.y -= num * 2;
 
 		return temp;
 	}
