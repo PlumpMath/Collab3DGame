962ed188cdef7ba1120bb36e84466bd569cf83bc
Scott Goerzen <sgoerzen@uvic.ca>
2017-04-13 09:12:52 -0700

    Changed health to 50 to increase difficulty

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -260,7 +260,7 @@ public class Player : MonoBehaviour {
     public void AffectHealth(float healthImpact)
     {
         health += healthImpact;
-		health = Mathf.Clamp(health, 0, 100);
+		health = Mathf.Clamp(health, 0, 50);
     }
 
 
